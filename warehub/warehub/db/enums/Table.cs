6d08fbc3f9a5c8583b9f8b9b0e62bec87e5f30ae
Capa2 <johan931@gmail.com>
2024-11-13 14:47:23 +0100

    add Crud and table enums

---
+++ warehub/warehub/db/enums/Table.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace warehub.db.enums
+{
+    internal class Table
+    {
+    }
+}
