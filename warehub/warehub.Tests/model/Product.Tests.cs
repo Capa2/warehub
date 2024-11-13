85f749db049dd4389356d3b0fdaa0323b88918a0
Capa2 <johan931@gmail.com>
2024-11-13 11:51:42 +0100

    add product, service tests

--- warehub/warehub.Tests/model/Product.Tests.cs
+++ warehub/warehub.Tests/model/Product.Tests.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
+using warehub.model;
+using System;
 
-namespace warehub.Tests.model
+namespace warehub.tests
 {
-    internal class Product
+    public class ProductTests
     {
+        [Fact]
+        public void Product_CreatesWithValidNameAndPrice()
+        {
+            // Arrange
+            string name = "Test Product";
+            int price = 100;
+
+            // Act
+            Product product = new Product(name, price);
+
+            // Assert
+            Assert.Equal(name, product.Name);
+            Assert.Equal(price, product.Price);
+            Assert.NotEqual(Guid.Empty, product.Id); // Check that the ID is generated
+        }
+
+        [Fact]
+        public void Product_CreatesWithSpecifiedGuid()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            string name = "Test Product";
+            int price = 200;
+
+            // Act
+            Product product = new Product(id, name, price);
+
+            // Assert
+            Assert.Equal(id, product.Id);
+            Assert.Equal(name, product.Name);
+            Assert.Equal(price, product.Price);
+        }
     }
 }
--- warehub/warehub.Tests/services/GuidService.Tests.cs
+++ warehub/warehub.Tests/services/GuidService.Tests.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
+using warehub.services;
+using System;
 
-namespace warehub.Tests.services
+namespace warehub.tests
 {
-    internal class GuidService
+    public class GuidServiceTests
     {
+        [Fact]
+        public void GuidService_GeneratesUniqueIds()
+        {
+            // Act
+            Guid id1 = GuidService.GenerateId();
+            Guid id2 = GuidService.GenerateId();
+
+            // Assert
+            Assert.NotEqual(id1, id2);
+            Assert.NotEqual(Guid.Empty, id1);
+            Assert.NotEqual(Guid.Empty, id2);
+        }
+
+        [Fact]
+        public void GuidService_ConvertsGuidToAndFromBinary()
+        {
+            // Arrange
+            Guid originalGuid = Guid.NewGuid();
+
+            // Act
+            byte[] binaryGuid = GuidService.GuidToBinary(originalGuid);
+            Guid convertedGuid = GuidService.BinaryToGuid(binaryGuid);
+
+            // Assert
+            Assert.Equal(originalGuid, convertedGuid);
+        }
+
+        [Fact]
+        public void GuidService_ConvertsGuidToAndFromString()
+        {
+            // Arrange
+            Guid originalGuid = Guid.NewGuid();
+
+            // Act
+            string guidString = GuidService.GuidToString(originalGuid);
+            Guid convertedGuid = GuidService.StringToGuid(guidString);
+
+            // Assert
+            Assert.Equal(originalGuid, convertedGuid);
+        }
     }
 }
